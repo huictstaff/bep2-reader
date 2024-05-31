16c341db2d81de41344195432740f69d1da7b8e1
Tom <tom.kemper@hu.nl>
2024-05-31 16:20:23 +0200

    Dubbele files opgeschoond, laten we eerst 1 versie maken, en dan de layout fixen

--- doc/materials/build-cursusopzet.cmd
+++
@@ -1,2 +0,0 @@
-call docker run -v %cd%/.:/src -w /src -it hutex:latest pdflatex -interaction=nonstopmode -output-directory target -shell-escape cursusopzet
-call docker run -v %cd%/.:/src -w /src -it hutex:latest pdflatex -interaction=nonstopmode -output-directory target -shell-escape cursusopzet
--- doc/materials/build-oop.cmd
+++
@@ -1,2 +0,0 @@
-call docker run -v %cd%:/src -w /src -it hutex:latest pdflatex -interaction=nonstopmode -output-directory target -shell-escape oop-in-java
-call docker run -v %cd%:/src -w /src -it hutex:latest pdflatex -interaction=nonstopmode -output-directory target -shell-escape oop-in-java
