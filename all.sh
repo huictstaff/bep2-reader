9a0b13a395ad50fa9e3fedef5822de07aa0a6dc5
Tom <tom.kemper@hu.nl>
2024-08-27 23:20:04 +0200

    Build all pdfs

--- all.sh
+++ all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd /doc
-pdflatex -interaction=nonstopmode -shell-escape bep2-reader
-bibtex bep2-reader
-makeindex bep2-reader
-pdflatex -interaction=nonstopmode -shell-escape bep2-reader
+cd /doc 
+for doc in bep2-reader bep2-reader-theorie bep2-reader-opdracht
+do
+    pdflatex -interaction=nonstopmode -shell-escape $doc
+    bibtex $doc
+    makeindex $doc
+    pdflatex -interaction=nonstopmode -shell-escape $doc
+done
